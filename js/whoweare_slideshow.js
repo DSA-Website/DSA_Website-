38edaf3476319e79a8e8b6f9c8be48913ec2cead
seg9985 <seg9985@gmail.com>
2025-07-13 12:34:26 -0500

    ads
    asd

---
+++ js/whoweare_slideshow.js
@@ -0,0 +1,21 @@
+let slideIndex = 0;
+const slides = document.getElementsByClassName("mySlides");
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+
+  slides[slideIndex - 1].classList.add("active");
+
+  setTimeout(showSlides, 2800); // ⏱️ 4 seconds per photo
+}
+
+// Show the first image instantly
+if (slides.length > 0) {
+  slides[0].classList.add("active");
+  setTimeout(showSlides, 2800);
+}
